1bf761d35172ece6d1db087aef640c7f2579c334
Alejandro <palebluedot@gmail.com>
2025-05-19 11:29:23 -0300

    Mod ejemplo simple

--- src/app/agentConfigs/simpleExample/index.ts
+++ src/app/agentConfigs/simpleExample/index.ts
@@ -2,24 +2,31 @@ import { AgentConfig } from '@/app/types';
 import { injectTransferTools } from '../utils';
 
 // Define agents
-const haikuWriter: AgentConfig = {
-  name: 'haikuWriter',
-  publicDescription: 'Agent that writes haikus.', // Context for the agent_transfer tool
+const rapero: AgentConfig = {
+  name: 'rapero',
+  publicDescription: 'Agente que inventa letras de rap', // Context for the agent_transfer tool
   instructions:
-    'Ask the user for a topic, then reply with a haiku about that topic.',
+    'Pidele al usuario un tema, y responde con una letra de rap sobre ese tema.',
+  tools: [],
+};
+
+const comediante: AgentConfig = {
+  name: 'comediante',
+  publicDescription: 'Agente que hace chistes',
+  instructions: 'Hace chistes al usuario',
   tools: [],
 };
 
 const greeter: AgentConfig = {
   name: 'greeter',
-  publicDescription: 'Agent that greets the user.',
+  publicDescription: 'Agente que recibe al usuario y lo deriva a otros agentes',
   instructions:
-    "Please greet the user and ask them if they'd like a Haiku. If yes, transfer them to the 'haiku' agent.",
+    "Recibe al usuario y preguntale que quiere hacer, si quiere hacer una letra de rap, deriva a la herramienta 'rapero', si quiere hacer un chiste, deriva a la herramienta 'comediante'",
   tools: [],
-  downstreamAgents: [haikuWriter],
+  downstreamAgents: [rapero, comediante],
 };
 
 // add the transfer tool to point to downstreamAgents
-const agents = injectTransferTools([greeter, haikuWriter]);
+const agents = injectTransferTools([greeter, rapero, comediante]);
 
 export default agents;
