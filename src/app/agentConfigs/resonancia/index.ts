23a56c952b041b489c8d2edfb40e041f458e3345
Alejandro <palebluedot@gmail.com>
2025-05-20 10:05:59 -0300

    Basic auth

--- src/app/agentConfigs/resonancia/index.ts
+++ src/app/agentConfigs/resonancia/index.ts
@@ -5,23 +5,23 @@ import { injectTransferTools } from '../utils';
 // Define agents
 const facilitador: AgentConfig = {
   name: 'facilitador',
-  publicDescription: 'Agente que facilita la experiencia de resonancia',
+  publicDescription: 'Agente que facilita la experiencia de resonancia.',
   instructions:
-    'Guia al usuario en una meditacion guiada. Al terminar derivalo al agente greeter.',
+    'Guia al usuario en una meditacion guiada. Al terminar derivalo al agente greeter. No saludes, simplemente confirma si esta listo para empezar la meditacion. Una vez que confirme, hazlo respirar unos segundos con los ojos cerrados, sentir su cuerpo, etc. Luego pidele que comparta que emocion siente. Habla lento y suavemente. Con una voz relajada y mistica.',
   tools: [],
 };
 
 const explainer: AgentConfig = {
   name: 'explainer',
   publicDescription: 'Agente que explica la experiencia de resonancia',
-  instructions: 'Explicale al usuario como funciona la experiencia de resonancia. Al terminar derivalo al agente greeter.',
+  instructions: 'Explicale al usuario como funciona la experiencia de resonancia. Al terminar derivalo al agente greeter. No preguntes como puedes ayudarlo, simplemente explica que resonancia es una experiencia de meditacion que guia al usuario a compartir una emocion. Luego esa emocion se convierte en un punto tridimencional y se proyecta en una animacion de un cosmos, en donde el usuario puede verse cerca de otros puntos y entender como resuena con otros. Habla de manera entusiasta y clara. Preguntale al usuario si quiere saber mas.',
   tools: [],
 };
 
 const admin: AgentConfig = {
   name: 'admin',
   publicDescription: 'Agente que permite administrar y configurar la experiencia de resonancia',
-  instructions: 'Brinda al usuario herramientas para configurar y administrar la experiencia de resonancia. Al finalizar la configuracion, deriva al usuario de nuevo al agente greeter.',
+  instructions: 'Brinda al usuario herramientas para configurar y administrar la experiencia de resonancia. Al finalizar la configuracion, deriva al usuario de nuevo al agente greeter. Habla en Ingles.',
   tools: [],
 };
 
@@ -29,7 +29,7 @@ const greeter: AgentConfig = {
   name: 'greeter',
   publicDescription: 'Agente que recibe al usuario y lo deriva a otros agentes',
   instructions:
-    'Recibe al usuario y derivalo al agente correspondiente. No menciones al agente admin, ya que solo es para usuarios avanzados.',
+    'Recibe al usuario y derivalo al agente correspondiente. Si el usuario quiere ser derivado al admin, pidele la palabra secreta: "gaviota". Si no la menciona, pidele disculpas y dile que no puedes transferirlo a ese agente.',
   tools: [],
 };
 
